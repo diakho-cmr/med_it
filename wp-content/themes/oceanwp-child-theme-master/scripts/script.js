bbe7b6a6acca0e7d827d40fcd1ec59a2c1a91a49
Diakho Camara <diakho.camara@gmail.com>
2023-05-05 10:54:47 +0200

    Remove popup on click on close button

--- wp-content/themes/oceanwp-child-theme-master/scripts/script.js
+++ wp-content/themes/oceanwp-child-theme-master/scripts/script.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+
+/** Remove popup on click on close button */
+const popup = document.querySelector('.popup-overlay');
+const popupCloseButton = document.querySelector('.popup-close');
+
+popupCloseButton.addEventListener('click', function() {
+    popup.classList.add('remove-popup');
+});
+/** End remove popup on click on close button */
